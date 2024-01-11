f2faad4b20500a595be0837e74cd4a89d58c943f
BradleyGilden <bradleygilden@gmail.com>
2024-01-11 12:01:29 +0200

    union annotation

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+a type-annotated function sum_mixed_list which takes a list mxd_lst of
+integers and floats and returns their sum as a float
+
+Author: Bradley Dillion Gilden
+Date: 11-01-2024
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
+    """
+    returns the sum of items of a list
+    """
+    return sum(input_list)
+
+
+if __name__ == '__main__':
+    print(sum_mixed_list.__annotations__)
+    mixed = [5, 4, 3.14, 666, 0.99]
+    ans = sum_mixed_list(mixed)
+    print(ans == sum(mixed))
+    print("sum_mixed_list(mixed) returns {} which is a {}"
+          .format(ans, type(ans)))
