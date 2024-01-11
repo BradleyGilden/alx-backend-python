9d4a4c1bd3d7233faabbbce3e0abddfae6bf220a
BradleyGilden <bradleygilden@gmail.com>
2024-01-11 09:42:34 +0200

    simple function annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 """
-<module docstring>
+a type-annotated function add that takes a float a and a float b as arguments
+and returns their sum as a float.
 
 Author: Bradley Dillion Gilden
 Date: 11-01-2024
 """
 
 
+def add(a: float, b: float) -> float:
+    return a + b
+
+
 if __name__ == '__main__':
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)
