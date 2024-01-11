0d7999fabca9660db17b9e8e720bfce55bea10fa
BradleyGilden <bradleygilden@gmail.com>
2024-01-11 09:49:03 +0200

    more function annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+A type-annotated function concat that takes a string str1 and a string str2
+as arguments and returns a concatenated string
+
+Author: Bradley Dillion Gilden
+Date: 11-01-2024
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates two strings (str1 + str2)
+    Returns: concatenated string
+    """
+    return str1 + str2
+
+
+if __name__ == '__main__':
+    str1 = "egg"
+    str2 = "shell"
+
+    print(concat(str1, str2) == "{}{}".format(str1, str2))
+    print(concat.__annotations__)
