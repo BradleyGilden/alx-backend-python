c34d464bd74a97d1556428d9a5917f1af8ae7914
BradleyGilden <bradleygilden@gmail.com>
2024-01-11 09:55:46 +0200

    more function annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+a type-annotated function floor which takes a float n as argument and
+returns the floor of the float.
+
+Author: Bradley Dillion Gilden
+Date: 11-01-2024
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Converts float n to an int
+    """
+    return int(n)
+
+
+if __name__ == '__main__':
+    ans = floor(3.14)
+
+    print(ans == math.floor(3.14))
+    print(floor.__annotations__)
+    print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
