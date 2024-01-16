2c2c773369a613cef538c5cc2b9d262c02030245
BradleyGilden <bradleygilden@gmail.com>
2024-01-16 12:46:21 +0200

    altered typing return

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,12 @@
 Author: Bradley Dillion Gilden
 Date: 16-01-2024
 """
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 from random import uniform
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> Generator[float, None, None]:
     """
     generates 10 coroutines, which sleep for 1 second and reutrn a random
     number between 0 and 10
